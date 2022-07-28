724bfaf7616d4760c819a0020b38eebdd41bcf5a
Kalvin <kalvinrasamy23@gmail.com>
2022-07-29 00:23:06 +0300

    travail terminé et on attend la derinière verification

--- Fiche_nom_pepiniere.php
+++ Fiche_nom_pepiniere.php
@@ -5,11 +5,11 @@ require_once 'connexion.php';
 $Fonction->logged_only();
 $Fonction->allow('member');
 
-$sql="SELECT * FROM nom_pepiniere
+$sql = "SELECT * FROM nom_pepiniere
 
     ORDER BY id_nompep ASC ";
 
-    $req=$db->query($sql);
+$req = $db->query($sql);
 
 ?>
 
@@ -26,108 +26,111 @@ require_once('includes/navbar.php');
 
 <!-- collaps -->
 
-    <?php 
-    if (isset($_SESSION['message'])): ?>
-    <div class="alert alert-<?=$_SESSION['msg_type']?>">
-        <?php 
-            echo $_SESSION['message'];
-            unset($_SESSION['message']);
+<?php
+if (isset($_SESSION['message'])) : ?>
+    <div class="alert alert-<?= $_SESSION['msg_type'] ?>">
+        <?php
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
         ?>
 
-    </div> 
-    
-    <?php endif ?>
-    <p></p>
+    </div>
+
+<?php endif ?>
+<p></p>
 <main class="container">
+    <div class="card shadow mb-4">
 
-    <?php 
-                        if ($update==true):
-                    ?>
+        <?php
+        if ($update == true) :
+        ?>
 
-                    <?php else : ?>
-                        <button class="btn btn-success" title="Nouveau nom pépinière" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                        <span class="fa fa-plus"></span>
-                        </button>
-                    <?php endif; ?>
+        <?php else : ?>
+            <button class="btn btn-success" title="Nouveau nom pépinière" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                <span class="fa fa-plus"></span>
+            </button>
+        <?php endif; ?>
 
-                    <?php 
-                        if ($update==true):
-                    ?>
-                    <div class="collapse show" id="collapseExample">
-                    <?php else : ?>
-                        <div class="collapse" id="collapseExample">
-                    <?php endif; ?>
+        <?php
+        if ($update == true) :
+        ?>
+            <div class="collapse show" id="collapseExample">
+            <?php else : ?>
+                <div class="collapse" id="collapseExample">
+                <?php endif; ?>
 
-<div class="card card-body">
-<div class="row justify-content-center">
-        <section class="col-3">
-            <form class="row g-4 was-validated" action="connexion.php" method="POST">
-                <input type="hidden" maxlength="4" name="id_nompep" value="<?php echo $id_nompep; ?>">
-                <div class="form-floating mb-3">     
-                    <label for="nom_pep">(*)Nom Pépinière</label>
-                    <input type="text" name="nom_pep" value="<?php echo $nom_pep; ?>" id="nom_pep" class="form-control" placeholder="Entrez le nom du pépinière" required>           
-                    <div class="invalid-feedback">le nom est incorrect</div>
-                    <div class="valid-feedback">le nom est correct</div>
+                <div class="card card-body">
+                    <div class="row justify-content-center">
+                        <section class="col-3">
+                            <form class="row g-4 was-validated" action="connexion.php" method="POST">
+                                <input type="hidden" maxlength="4" name="id_nompep" value="<?php echo $id_nompep; ?>">
+                                <div class="form-floating mb-3">
+                                    <label for="nom_pep">(*)Nom Pépinière</label>
+                                    <input type="text" name="nom_pep" value="<?php echo $nom_pep; ?>" id="nom_pep" class="form-control" placeholder="Entrez le nom du pépinière" required>
+                                    <div class="invalid-feedback">le nom est incorrect</div>
+                                    <div class="valid-feedback">le nom est correct</div>
+                                </div>
+
+                                <div class="form-floating mb-3">
+                                    <div class="form-group">
+                                        <?php
+                                        if ($update == true) :
+                                        ?>
+                                            <button type="submit" class="btn btn-primary" name="update" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Modifier</button>
+                                            <a href="Fiche_nom_pepiniere.php" type="submit" class="btn btn-danger">Annuler</a>
+                                        <?php else : ?>
+                                            <button type="submit" class="btn btn-primary" name="save" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Ajouter</button>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+
+
+                            </form>
+                        </section>
+                    </div>
+                </div>
+                </div>
+                <?php
+                $mysqli = new mysqli('localhost', 'root', '', 'reboisement') or die(mysqli_error($mysqli));
+                $result = $mysqli->query("SELECT * FROM nom_pepiniere ORDER BY id_nompep DESC") or die($mysqli->error);
+                ?>
+
+                <h2 class=" text-primary text-center " style="margin-top: 40px; ">Liste des Pépinière</h2>
+                <div class="row">
+                    <table class="table">
+                        <thead class="bg-dark text-light text-center">
+                            <tr>
+                                <th>N°</th>
+                                <th>Nom Pépinière</th>
+                                <th colspan="2">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-center">
+                            <?php
+                            while ($row = $result->fetch_assoc()) : ?>
+                                <tr>
+                                    <td><?php echo $row['id_nompep']; ?></td>
+                                    <td><?php echo $row['nom_pep']; ?></td>
+                                    <td>
+                                        <a href="Fiche_nom_pepiniere.php?edit=<?php echo $row['id_nompep']; ?>" class="badge badge-pill badge-success">Modifier</a>
+                                        <a href="connexion.php?delete=<?php echo $row['id_nompep']; ?>" class="badge badge-pill badge-danger" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Supprimer</a>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
                 </div>
 
-                <div class="form-floating mb-3"> 
-                <div class="form-group">
-                    <?php 
-                        if ($update==true):
-                    ?>
-                    <button type="submit" class="btn btn-primary" name="update" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Modifier</button>
-                    <a href="Fiche_nom_pepiniere.php" type="submit" class="btn btn-danger">Annuler</a>
-                    <?php else : ?>
-                    <button type="submit" class="btn btn-primary" name="save" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Ajouter</button>
-                    <?php endif; ?>
-                </div>
-                </div>
-                
+                <?php
+                function pre_r($array)
+                {
+                    echo '<pre>';
+                    print_r($array);
+                    echo '</pre>';
+                }
 
-            </form>
-        </section>
-    </div>
-    </div>
-    </div>
-    <?php 
-    $mysqli = new mysqli('localhost','root','','reboisement') or die(mysqli_error($mysqli));
-    $result = $mysqli->query("SELECT * FROM nom_pepiniere") or die($mysqli->error); 
-    ?>
-    
-    <h2 class=" text-primary text-center " style="margin-top: 40px; ">Liste des Pépinière</h2>
-    <div class="row">
-        <table class="table">
-            <thead class="bg-dark text-light text-center">
-                <tr>
-                    <th>N°</th>
-                    <th>Nom Pépinière</th>
-                    <th colspan="2">Action</th>
-                </tr>
-            </thead>
-            <tbody class="text-center">
-            <?php 
-                while ($row = $result->fetch_assoc()): ?>
-                <tr>
-                    <td><?php echo $row['id_nompep']; ?></td>
-                    <td><?php echo $row['nom_pep']; ?></td>
-                    <td>
-                        <a href="Fiche_nom_pepiniere.php?edit=<?php echo $row['id_nompep']; ?>" class="badge badge-pill badge-success">Modifier</a>
-                        <a href="connexion.php?delete=<?php echo $row['id_nompep']; ?>" class="badge badge-pill badge-danger" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Supprimer</a>
-                    </td>
-                </tr>
-            <?php endwhile; ?>
-            </tbody>
-        </table>
-    </div>
-
-    <?php
-    function pre_r( $array){
-        echo '<pre>';
-        print_r($array);
-        echo '</pre>';
-    }
-
-?>
+                ?>
+            </div>
 </main>
 
 
--- type_acteur.php
+++ type_acteur.php
@@ -6,11 +6,11 @@ $Fonction->logged_only();
 $Fonction->allow('member');
 
 
-$sql="SELECT * FROM type_acteur
+$sql = "SELECT * FROM type_acteur
 
     ORDER BY ID_TYPE_ACTEUR ASC ";
 
-    $req=$db->query($sql);
+$req = $db->query($sql);
 ?>
 
 
@@ -26,117 +26,120 @@ require_once('includes/navbar.php');
 
 <!-- collaps -->
 
-    <?php 
-    if (isset($_SESSION['message'])): ?>
-    <div class="alert alert-<?=$_SESSION['msg_type']?>">
-        <?php 
-            echo $_SESSION['message'];
-            unset($_SESSION['message']);
+<?php
+if (isset($_SESSION['message'])) : ?>
+    <div class="alert alert-<?= $_SESSION['msg_type'] ?>">
+        <?php
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
         ?>
 
-    </div> 
-    
-    <?php endif ?>
-    <p></p>
+    </div>
+
+<?php endif ?>
+<p></p>
 <main class="container">
+    <div class="card shadow mb-4">
 
-    <?php 
-                        if ($update==true):
-                    ?>
+        <?php
+        if ($update == true) :
+        ?>
 
-                    <?php else : ?>
-                        <button class="btn btn-success" title="Nouveau type acteur" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                        <span class="fa fa-plus"></span>
-                        </button>
-                    <?php endif; ?>
+        <?php else : ?>
+            <button class="btn btn-success" title="Nouveau type acteur" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                <span class="fa fa-plus"></span>
+            </button>
+        <?php endif; ?>
 
-                    <?php 
-                        if ($update==true):
-                    ?>
-                    <div class="collapse show" id="collapseExample">
-                    <?php else : ?>
-                        <div class="collapse" id="collapseExample">
-                    <?php endif; ?>
+        <?php
+        if ($update == true) :
+        ?>
+            <div class="collapse show" id="collapseExample">
+            <?php else : ?>
+                <div class="collapse" id="collapseExample">
+                <?php endif; ?>
 
-<div class="card card-body">
-<div class="row justify-content-center">
-        <section class="col-3">
-            <form class="row g-4 was-validated" action="conn_type_acteur.php" method="POST">
-                <input type="hidden" name="ID_TYPE_ACTEUR" value="<?php echo $ID_TYPE_ACTEUR; ?>">
-                <div class="form-floating mb-3">     
-                    <label for="LIBELLETYPE_ACTEUR">(*)Libelle type acteur :</label>
-                    <input type="text" name="LIBELLETYPE_ACTEUR" value="<?php echo $LIBELLETYPE_ACTEUR; ?>" id="LIBELLETYPE_ACTEUR" class="form-control" required>           
-                    <div class="invalid-feedback">le nom est incorrect</div>
-                    <div class="valid-feedback">le nom est correct</div>
+                <div class="card card-body">
+                    <div class="row justify-content-center">
+                        <section class="col-3">
+                            <form class="row g-4 was-validated" action="conn_type_acteur.php" method="POST">
+                                <input type="hidden" name="ID_TYPE_ACTEUR" value="<?php echo $ID_TYPE_ACTEUR; ?>">
+                                <div class="form-floating mb-3">
+                                    <label for="LIBELLETYPE_ACTEUR">(*)Libelle type acteur :</label>
+                                    <input type="text" name="LIBELLETYPE_ACTEUR" value="<?php echo $LIBELLETYPE_ACTEUR; ?>" id="LIBELLETYPE_ACTEUR" class="form-control" required>
+                                    <div class="invalid-feedback">le nom est incorrect</div>
+                                    <div class="valid-feedback">le nom est correct</div>
+                                </div>
+
+                                <div class="form-floating mb-3">
+                                    <label for="DESCRIPTION">Description : </label>
+                                    <input type="text" name="DESCRIPTION" value="<?php echo $DESCRIPTION; ?>" id="DESCRIPTION" class="form-control">
+                                    <div class="invalid-feedback">le nom est incorrect</div>
+                                    <div class="valid-feedback">le nom est correct</div>
+                                </div>
+
+                                <div class="form-floating mb-3">
+                                    <div class="form-group">
+                                        <?php
+                                        if ($update == true) :
+                                        ?>
+                                            <button type="submit" class="btn btn-primary" name="update" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Modifier</button>
+                                            <a href="type_acteur.php" type="submit" class="btn btn-danger">Annuler</a>
+                                        <?php else : ?>
+                                            <button type="submit" class="btn btn-primary" name="save" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Ajouter</button>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+
+
+                            </form>
+                        </section>
+                    </div>
+                </div>
+                </div>
+                <?php
+                $mysqli = new mysqli('localhost', 'root', '', 'reboisement') or die(mysqli_error($mysqli));
+                $result = $mysqli->query("SELECT * FROM type_acteur ORDER BY ID_TYPE_ACTEUR DESC") or die($mysqli->error);
+                ?>
+
+                <h2 class=" text-primary text-center " style="margin-top: 40px; ">Liste des types acteurs</h2>
+                <div class="row">
+                    <table class="table">
+                        <thead class="bg-dark text-light text-center">
+                            <tr>
+                                <th>N°</th>
+                                <th>Libelle type acteur</th>
+                                <th>Description</th>
+                                <th colspan="2">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-center">
+                            <?php
+                            while ($row = $result->fetch_assoc()) : ?>
+                                <tr>
+                                    <td><?php echo $row['ID_TYPE_ACTEUR']; ?></td>
+                                    <td><?php echo $row['LIBELLETYPE_ACTEUR']; ?></td>
+                                    <td><?php echo $row['DESCRIPTION']; ?></td>
+                                    <td>
+                                        <a href="type_acteur.php?edit=<?php echo $row['ID_TYPE_ACTEUR']; ?>" class="badge badge-pill badge-success">Modifier</a>
+                                        <a href="conn_type_acteur.php?delete=<?php echo $row['ID_TYPE_ACTEUR']; ?>" class="badge badge-pill badge-danger" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Supprimer</a>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
                 </div>
 
-                <div class="form-floating mb-3">     
-                    <label for="DESCRIPTION">Description : </label>
-                    <input type="text" name="DESCRIPTION" value="<?php echo $DESCRIPTION; ?>" id="DESCRIPTION" class="form-control">           
-                    <div class="invalid-feedback">le nom est incorrect</div>
-                    <div class="valid-feedback">le nom est correct</div>
-                </div>
+                <?php
+                function pre_r($array)
+                {
+                    echo '<pre>';
+                    print_r($array);
+                    echo '</pre>';
+                }
 
-                <div class="form-floating mb-3"> 
-                <div class="form-group">
-                    <?php 
-                        if ($update==true):
-                    ?>
-                    <button type="submit" class="btn btn-primary" name="update" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Modifier</button>
-                    <a href="type_acteur.php" type="submit" class="btn btn-danger">Annuler</a>
-                    <?php else : ?>
-                    <button type="submit" class="btn btn-primary" name="save" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Ajouter</button>
-                    <?php endif; ?>
-                </div>
-                </div>
-                
-
-            </form>
-        </section>
-    </div>
-    </div>
-    </div>
-    <?php 
-    $mysqli = new mysqli('localhost','root','','reboisement') or die(mysqli_error($mysqli));
-    $result = $mysqli->query("SELECT * FROM type_acteur") or die($mysqli->error); 
-    ?>
-    
-    <h2 class=" text-primary text-center " style="margin-top: 40px; ">Liste des types acteurs</h2>
-    <div class="row">
-        <table class="table">
-            <thead class="bg-dark text-light text-center">
-                <tr>
-                    <th>N°</th>
-                    <th>Libelle type acteur</th>
-                    <th>Description</th>
-                    <th colspan="2">Action</th>
-                </tr>
-            </thead>
-            <tbody class="text-center">
-            <?php 
-                while ($row = $result->fetch_assoc()): ?>
-                <tr>
-                    <td><?php echo $row['ID_TYPE_ACTEUR']; ?></td>
-                    <td><?php echo $row['LIBELLETYPE_ACTEUR']; ?></td>
-                    <td><?php echo $row['DESCRIPTION']; ?></td>
-                    <td>
-                        <a href="type_acteur.php?edit=<?php echo $row['ID_TYPE_ACTEUR']; ?>" class="badge badge-pill badge-success">Modifier</a>
-                        <a href="conn_type_acteur.php?delete=<?php echo $row['ID_TYPE_ACTEUR']; ?>" class="badge badge-pill badge-danger" onclick="return(confirm('Etes-vous sûr de vouloir confirmer la commande'));">Supprimer</a>
-                    </td>
-                </tr>
-            <?php endwhile; ?>
-            </tbody>
-        </table>
-    </div>
-
-    <?php
-    function pre_r( $array){
-        echo '<pre>';
-        print_r($array);
-        echo '</pre>';
-    }
-
-?>
+                ?>
+            </div>
 </main>
 
 
